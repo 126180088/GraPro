b99a99d8aa2a511811aa8e02f3976243554d65c3
126180088 <42297731+126180088@users.noreply.github.com>
2021-12-12 02:32:33 +0800

    V1

---
+++ GraPro/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraPro.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// 登陆
+        /// </summary>
+        [HttpGet]
+        public IActionResult SignIn(int id)
+        {
+            return Ok("success");
+        }
+    }
+}
