28331f5e3a172db251b93e305e78d6682d62f936
126180088 <42297731+126180088@users.noreply.github.com>
2021-12-13 00:50:32 +0800

    V1

---
+++ GraPro/Helpper/DataResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JWT.MvcDemo.Models
+{
+    public class DataResult
+    {
+        /// <summary>
+        /// 消息返回类
+        /// </summary>
+        public string Token { get; set; }
+
+        public object Data { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
---
+++ GraPro/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace GraPro.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Pwd { get; set; }
+    }
+}
