2ecd53eef2ea38b0587c40a614b29af171578ac3
126180088 <770271967@qq.com>
2021-12-21 17:20:20 +0800

    后端页面设置中

--- GraPro/Startup.cs
+++ GraPro/Startup.cs
@@ -20,6 +20,10 @@ namespace GraPro
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -45,13 +49,18 @@ namespace GraPro
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+
                 app.UseSwagger();
+
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GraPro v1"));
+
                 app.UseDeveloperExceptionPage();
+
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -59,9 +68,14 @@ namespace GraPro
 
             //配置Cors
             app.UseCors("any");
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers().RequireCors("any");
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
